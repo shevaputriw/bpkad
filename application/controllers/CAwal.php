07ed50304fc63e06d5a593957713b436d7bc7b7f
shevaputriw <shevapw@gmail.com>
2021-10-05 12:22:19 +0700

    new

--- application/controllers/CAwal.php
+++ application/controllers/CAwal.php
@@ -4,12 +4,6 @@
 
     class CAwal extends CI_Controller {
 
-        public function __construct()
-        {
-            parent::__construct();
-            
-        }
-
         public function index() {
             $data['title'] = 'Welcome';
 
