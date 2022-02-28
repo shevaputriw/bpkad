1e6c008ecedeb2e3bb72e316413cb754df0583bf
IlhamYanto <mohilhamfebriyanto.7a.14@gmail.com>
2022-02-28 09:14:26 +0700

    Fix Button

--- application/views/Menu.php
+++ application/views/Menu.php
@@ -18,6 +18,15 @@
 	body{	
 		background-color: transparent;
 	}
+	.btn-sewa {
+    	background-color: #ed6f28 !important;
+    	border-color: #ed6f28 !important;
+	}
+
+	.btn-sewa:hover {
+	    background-color: #98471a !important;
+	    border-color: #ed5b28 !important;
+	}
 </style>
 <body>
 	<div class="container">
@@ -87,14 +96,14 @@
 							</a>
 							<div class="card-footer p-0">
 								<div class="btn-group" style="width:100%">
-									<button type="button" style="width:27%" class="btn">&nbsp;</button>
-									<button type="button" style="width:23%" class="btn btn-warning" >
+									<button type="button" style="width:27.6%" class="btn p-0">&nbsp;</button>
+									<button type="button" style="width:24%" class="btn p-0 btn-warning btn-sewa" style="border-bottom-left-radius: 5px;">
 										<a href="<?=base_url()?>CAwal/Login" style="color:white"><b>2021</b></a>
 									</button>
-									<button type="button" style="width:23%" class="btn btn-warning">
+									<button type="button" style="width:24%" class="btn p-0 btn-warning btn-sewa" style="border-bottom-right-radius: 5px">
 										<a href="<?=base_url()?>CAwal/Login" style="color:white"><b>2022</b></a>
 									</button>
-									<button type="button" style="width:27%" class="btn">&nbsp;</button>
+									<button type="button" style="width:24.4%" class="btn p-0">&nbsp;</button>
 								</div>
 							</div>
 						</div>
